57a87c7f2b1c5468e8b07f6fda8729b5513806d3
Philipp <admin@n3rd3d.com>
2024-09-20 18:59:46 +0200

    Update codecs.go

--- core/transcoder/codecs.go
+++ core/transcoder/codecs.go
@@ -191,7 +191,7 @@ func (c *VaapiCodec) GlobalFlags() string {
 
 // PixelFormat is the pixel format required for this codec.
 func (c *VaapiCodec) PixelFormat() string {
-	return "vaapi -rc_mode CQP -qp 18"
+	return "vaapi -rc_mode CQP"
 }
 
 // Scaler is the scaler used for resizing the video in the transcoder.
